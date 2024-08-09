823d199aab587d6b9e1976e631a4a967407a8c5b
John Galt <galt@fedora.com>
2024-08-09 18:43:18 +0200

    adds type RouteMatcher

--- server/route.go
+++ server/route.go
@@ -1,4 +1,4 @@
-package server 
+package server
 
 import (
 	"net"
@@ -7,6 +7,10 @@ import (
 // HandlerFunc defines a function type for handling Gemini request
 type HandlerFunc func(path string, conn net.Conn)
 
+// Define the RouteMatcher type
+type RouteMatcher func(path string, route Route) bool
+
+// Define the Route struct
 type Route struct {
 	Path    string
 	Handler HandlerFunc
